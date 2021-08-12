6a93302a8c8f61a569c557148c1a955627a754da
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-12 01:58:51 +0000

    Removed all the useless comments

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// Arrays to keep track of each task's state
-// const taskTitles = [];
-// const taskComplete = [];
-// const taskDescriptions = [];
-
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-
 function newTask(title, description) {
 
   const task = {
@@ -23,16 +15,6 @@ function newTask(title, description) {
   return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-// function completeTask(task) {
-//   task.complete = true;
-// }
-
-// // Print the state of a task to the console in a nice readable way
-// function logTaskState(task) {
-//   console.log(`${task.title} has${task.complete ? " " : " not"}been completed`)
-// }
-
 // DRIVER CODE BELOW:
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box"); //task[0]
@@ -49,9 +31,3 @@ task2.logState();
 task2.markCompleted();
 task2.logState();
 
-//ensure we see our tasks
-//console.log(tasks);
-
-// logTaskState(0); // Clean Cat Litter has not been completed
-// completeTask(0);
-// logTaskState(0); // Clean Cat Litter has been completed
